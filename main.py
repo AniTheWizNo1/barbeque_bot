b9ccf813797c5a59f4d9dc836da3b87e3ec2aee8
Aniket Das <aniket10das@gmail.com>
2025-05-18 18:31:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,3 +51,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Render provides PORT env variable
+    uvicorn.run(app, host="0.0.0.0", port=port)
